a670e4cbc09a243e84fd3aefb7872eb6475b4652
Neamul Islam <neamul909976@gmail.com>
2023-12-20 23:00:40 +0600

    connection string

--- TaskManagement/obj/Debug/net8.0/TaskManagement.AssemblyInfo.cs
+++ TaskManagement/obj/Debug/net8.0/TaskManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5dc4897a2212add464b30f22f113ffd3c34a664c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
